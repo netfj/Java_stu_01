95f93461c4928ffceae55bbb85100cdfa837fa10
netfj <netfj@sina.com>
2021-02-18 14:12:01 +0800

    2021-2-18 II

--- src/cn/itcast/chapter01/HelloWorld.java
+++ src/cn/itcast/chapter01/HelloWorld.java
@@ -1,8 +1,10 @@
 package cn.itcast.chapter01;
+import cn.itcast.chapterdemo.Test;
 
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello world !");
+		System.out.println("Hello world !");		
 	}
+	
 
 }
--- src/cn/itcast/chapterdemo/Test.java
+++ src/cn/itcast/chapterdemo/Test.java
@@ -1,5 +1,9 @@
 package cn.itcast.chapterdemo;
 
 public class Test {
+	public static void main (String[] args) {
+		System.out.println("I am Test !");
+	}
 
 }
+
