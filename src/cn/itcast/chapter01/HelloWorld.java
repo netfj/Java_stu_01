3b5d23250d38bec95d20b1b1995ebc55043a13cc
netfj <netfj@sina.com>
2021-02-18 14:20:57 +0800

    2012-2-18 IV

--- src/cn/itcast/chapter01/HelloWorld.java
+++ src/cn/itcast/chapter01/HelloWorld.java
@@ -3,7 +3,7 @@ import cn.itcast.chapterdemo.Test;
 
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello world !");		
+		System.out.println("Hello world ! git test!");		
 	}
 	
 
