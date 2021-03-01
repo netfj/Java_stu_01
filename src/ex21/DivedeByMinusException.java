bf9f7a33f49b92f68d9e5821cd06798004042a78
netfj <netfj@sina.com>
2021-03-01 19:58:02 +0800

    2021年3月1日

---
+++ src/ex21/DivedeByMinusException.java
@@ -0,0 +1,14 @@
+package ex21;
+
+public class DivedeByMinusException extends Exception{
+
+	public DivedeByMinusException() {
+		super();
+	}
+	
+	public DivedeByMinusException(String message) {
+		super(message);
+		// TODO 自动生成的构造函数存根
+	}
+	
+}
---
+++ src/ex22/Ex22.java
@@ -0,0 +1,14 @@
+package ex22;
+
+public class Ex22 {
+	public static void main(String[] args) {
+		StringBuffer sBuffer = new StringBuffer("012ab67890");
+		System.out.println(sBuffer);
+		sBuffer =new  StringBuffer("abc");
+		System.out.println(sBuffer);
+		
+		
+		
+		
+	}
+}
